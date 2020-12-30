0cf6c87aae225651d3bfd4b52b4088ead97ba24f
sztuczkap <p.sztuczka@wp.pl>
2020-12-30 18:00:45 +0100

    interfejs Operator

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,8 +1,17 @@
 package pl.sztuczkap;
 
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // przyjmujemy i zwracamy ten sam tym pomnozony przez 2
+        UnaryOperator<Integer> unaryOperator = numer -> numer * 2;
+        System.out.println(unaryOperator.apply(5));
+
+        BinaryOperator<Integer> binaryOperator = Integer::sum; // (i1, i2) -> i1+ i2;
+        System.out.println(binaryOperator.apply(10, 15));
     }
 }
